81cebdac6db0bcdf846100940f079c7d4d6085a0
Chantal Justamond <cjustamm66@me.com>
2017-08-02 17:12:44 -0600

    bootstrap added, settings for users enabled, devise integration wip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  # to properly generate links inside email views
+  config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
